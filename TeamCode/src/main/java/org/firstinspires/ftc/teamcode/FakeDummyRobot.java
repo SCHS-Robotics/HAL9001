333dd343142690b25994afdcd5bf92babdab46d5
Cole Savage <colesavage02@gmail.com>
2020-05-06 21:52:31 -0700

    Removed annotation dependency for teamcode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FakeDummyRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FakeDummyRobot.java
@@ -3,10 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.SCHSRobotics.HAL9001.system.source.BaseRobot.Robot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.jetbrains.annotations.NotNull;
 
 public class FakeDummyRobot extends Robot {
-    public FakeDummyRobot(@NotNull OpMode opMode) {
+    public FakeDummyRobot(OpMode opMode) {
         super(opMode);
     }
 }
