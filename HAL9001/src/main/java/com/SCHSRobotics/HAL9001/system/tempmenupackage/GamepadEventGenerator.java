abfcb42fbdd52589c95e1d1c0d932ac9f6dfaca1
Cole Savage <colesavage02@gmail.com>
2020-05-17 14:58:22 -0700

    Added waitTime timer

--- HAL9001/src/main/java/com/SCHSRobotics/HAL9001/system/tempmenupackage/GamepadEventGenerator.java
+++ HAL9001/src/main/java/com/SCHSRobotics/HAL9001/system/tempmenupackage/GamepadEventGenerator.java
@@ -9,10 +9,12 @@ import java.util.Map;
 
 public class GamepadEventGenerator {
     private static final int DEFAULT_BUTTON_EVENT_PRIORITY = 0;
+    private static final long WHILE_CLICKED_WAIT_TIME_MS = 100;
 
     private CustomizableGamepad gamepad;
     private Map<Button<Boolean>, Boolean> lastState;
     private Map<Button<Boolean>, Integer> priorities;
+    private Map<Button<Boolean>, Timer> whileClickTimers;
     private static GamepadEventGenerator INSTANCE = new GamepadEventGenerator();
     private GamepadEventGenerator() {
         reset();
@@ -33,18 +35,24 @@ public class GamepadEventGenerator {
                 boolean currentValue = gamepad.getInput(booleanButton);
                 Boolean lastValueBoxed = lastState.get(booleanButton);
                 ExceptionChecker.assertNonNull(lastValueBoxed, new NullPointerException("returned value from gamepad was null, this shouldn't be possible."));
+
                 boolean lastValue = lastValueBoxed;
                 Integer boxedPriority = priorities.get(booleanButton);
                 ExceptionChecker.assertNonNull(boxedPriority, new NullPointerException("Priority for button was null, this should be impossible."));
                 int priority = boxedPriority;
+
+                Timer timer = whileClickTimers.get(booleanButton);
+                ExceptionChecker.assertNonNull(timer, new NullPointerException("Timer was null, this should not be possible."));
+
                 if (!lastValue && currentValue) {
                     Event.injectEvent(new OnClickEvent(priority, booleanButton));
                 }
                 if (lastValue && !currentValue) {
                     Event.injectEvent(new OnClickReleaseEvent(priority, booleanButton));
                 }
-                if (currentValue) {
+                if (currentValue && timer.requiredTimeElapsed()) {
                     Event.injectEvent(new WhileClickEvent(priority, booleanButton));
+                    timer.start(WHILE_CLICKED_WAIT_TIME_MS, TimeUnit.MILLISECONDS);
                 }
 
                 lastState.put(booleanButton, currentValue);
@@ -56,6 +64,7 @@ public class GamepadEventGenerator {
         gamepad = new CustomizableGamepad(HALGUI.getInstance().getRobot());
         lastState = new HashMap<>();
         priorities = new HashMap<>();
+        whileClickTimers = new HashMap<>();
         for(Button.BooleanInputs booleanButtonType : Button.BooleanInputs.values()) {
             Button<Boolean> button1 = new Button<>(1, booleanButtonType);
             Button<Boolean> button2 = new Button<>(2, booleanButtonType);
@@ -64,6 +73,8 @@ public class GamepadEventGenerator {
             lastState.put(button2, false);
             priorities.put(button1, DEFAULT_BUTTON_EVENT_PRIORITY);
             priorities.put(button2, DEFAULT_BUTTON_EVENT_PRIORITY);
+            whileClickTimers.put(button1, new Timer());
+            whileClickTimers.put(button2, new Timer());
         }
     }
 }
