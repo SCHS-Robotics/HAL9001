b751842dbf805ab8ee74515460b0f7153fb255a4
Cole Savage <colesavage02@gmail.com>
2020-10-02 01:02:48 -0700

    Removed a comment from FakeNumpyTest.java

--- HAL9001/src/test/java/FakeNumpyTest.java
+++ HAL9001/src/test/java/FakeNumpyTest.java
@@ -473,8 +473,6 @@ public class FakeNumpyTest {
         FakeNumpy.divide(testArr,0.0);
     }
 
-    //------
-
     @Test
     public void validateAbsDoubleNonEmpty() {
         double[] testArr = new double[] {-6.6,-5.5,-4.4,-3.3,-2.2,-1.1,0.1,0.0,1.1,1.1,2.2,3.3,4.4,5.5,6.6,6.0};
